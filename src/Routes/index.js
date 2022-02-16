b5830547747267914998619869d67aa278448936
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-02-15 23:37:01 -0300

    Modificando cores do drawer

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -8,7 +8,21 @@ const Drawer = createDrawerNavigator();
 
 function Routes(){
     return(
-        <Drawer.Navigator>
+        <Drawer.Navigator
+            screenOptions={{
+                headerShown:false,
+                
+                drawerStyle:{
+                    backgroundColor: '#090a0e',
+                    paddingTop: 20,
+                },
+
+                drawerActiveBackgroundColor: '#e72f49',
+                drawerActiveTintColor: '#fff',
+                drawerInactiveTintColor: '#fff',
+            }}
+
+        >
             <Drawer.Screen name="HomeDrawer" component={StackRoutes} />
             <Drawer.Screen name="Movies" component={Movies} />
         </Drawer.Navigator>
