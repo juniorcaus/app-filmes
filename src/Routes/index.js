4024886f4744687bb012ed27fd3851349899b5ed
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-02-16 22:43:28 -0300

    Adicionando routes e header das páginas

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import Movies from '../pages/Movies';
-import StackRoutes from './StackRoutes';
+import StackRoutes from './stackRoutes';
 
 const Drawer = createDrawerNavigator();
 
@@ -16,15 +17,41 @@ function Routes(){
                     backgroundColor: '#090a0e',
                     paddingTop: 20,
                 },
-
                 drawerActiveBackgroundColor: '#e72f49',
                 drawerActiveTintColor: '#fff',
                 drawerInactiveTintColor: '#fff',
             }}
-
         >
-            <Drawer.Screen name="HomeDrawer" component={StackRoutes} />
-            <Drawer.Screen name="Movies" component={Movies} />
+            <Drawer.Screen
+             name="HomeDrawer" 
+             component={StackRoutes} 
+             options={{
+                 title: 'Home',
+                 drawerIcon: ({ focused, size, color }) => (
+                    <MaterialCommunityIcons 
+                        name={focused ? 'movie-open' : 'movie-outline'}
+                        size={size}
+                        color={color} 
+                    />   
+                 )
+             }}
+             />
+            <Drawer.Screen
+             name="Movies" 
+             component={Movies} 
+             options={{
+                 title: 'Meus filmes',
+                 drawerIcon: ({focused, size, color }) => (
+                    <MaterialCommunityIcons
+                        name={focused ? 'archive' : 'archive-outline' }
+                        size={size}
+                        color={color}
+                    />
+
+                 )
+
+             }}
+             />
         </Drawer.Navigator>
     )
 }
