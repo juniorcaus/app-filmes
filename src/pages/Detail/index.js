cabd302068242a0484e92a29d0f107859042d7a6
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-02-21 21:17:41 -0300

    Criando caminhos da nova pagina de detalhes

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,10 +1,67 @@
-import React from "react";
-import { Container, Title } from './styles';
+import React, { useState, useEffect } from "react";
+import { Container, Header, HeaderButton, Banner  } from './styles';
+
+import { Feather, Ionicons } from '@expo/vector-icons';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import api, { key } from '../../services/api';
 
 function Detail(){
+    const navigation = useNavigation();
+    const route = useRoute();
+
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        let isActive = true;
+
+        async function getMovie(){
+            const response = await api.get(`/movie/${route.params?.id}`, {
+                params:{
+                    api_key: key,
+                    language: 'pt-BR',
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            } )
+
+            if(isActive){
+                setMovie(response.data);
+                console.log(response.data);
+            }
+
+        }
+
+        if(isActive){
+            getMovie();
+        }
+        
+        return () => {
+            isActive = false;
+        }
+
+    }, [] )
+
     return(
         <Container>
-            <Title>TESTE DETAIL</Title>
+            <Header>
+                <HeaderButton>
+                    <Feather  
+                        name="arrow-left"
+                        size={28}
+                        color="#fff"
+                    />
+                </HeaderButton>
+                <HeaderButton>
+                    <Ionicons
+                        name="bookmark"
+                        size={28}
+                        color="#fff"
+                    />
+                </HeaderButton>
+            </Header>
+
+
         </Container>
     )
 }
