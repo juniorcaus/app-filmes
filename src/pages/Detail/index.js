0a323b2902b55e678fcb8cb3ff3fa54960545194
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-02-22 22:01:54 -0300

    Adicionando Votação e categorias na página de detalhes

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { Container, Header, HeaderButton, Banner  } from './styles';
+import { Container, Header, HeaderButton, Banner, ButtonLink, Title, ContentArea, Rate, ListGenres  } from './styles';
 
 import { Feather, Ionicons } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import api, { key } from '../../services/api';
+
+import Stars from 'react-native-stars';
+import Genres from "../../components/Genres";
 
 function Detail(){
     const navigation = useNavigation();
@@ -27,7 +30,7 @@ function Detail(){
 
             if(isActive){
                 setMovie(response.data);
-                console.log(response.data);
+                //console.log(response.data);
             }
 
         }
@@ -45,7 +48,7 @@ function Detail(){
     return(
         <Container>
             <Header>
-                <HeaderButton>
+                <HeaderButton activeOpacity={0.7} onPress={ () => navigation.goBack() } >
                     <Feather  
                         name="arrow-left"
                         size={28}
@@ -60,6 +63,40 @@ function Detail(){
                     />
                 </HeaderButton>
             </Header>
+
+            <Banner
+            resizeMethod="resize"
+            source={{ uri: `https://image.tmdb.org/t/p/original/${movie.poster_path}` }}
+
+            />
+
+            <ButtonLink>
+                <Feather name="link" size={24} color="#fff" />
+            </ButtonLink>
+
+            <Title numberOfLines={2} >{movie.title}</Title>
+
+            <ContentArea>
+                <Stars 
+                  default={movie.vote_average}  
+                  count={10}
+                  half={true}
+                  starSize={20}
+                  fullStar={ <Ionicons name="md-star" size={24} color="#E7A74E" /> }
+                  emptyStart={ <Ionicons name="md-star-outline" size={24} color="#E7A74E" /> }
+                  halfStar={ <Ionicons name="md-star-half" size={24} color="#E7A74E" /> }
+                  disable={true}
+                />
+                <Rate>{movie.vote_average} /10</Rate>
+            </ContentArea>
+
+            <ListGenres
+            data={movie?.genres}
+            horizontal={true}
+            shownsHorizontalScrollIndicator={false}
+            keyExtractor={ (item) => String(item.id) }
+            renderItem={ ({ item }) => <Genres data={item} /> }
+            />
 
 
         </Container>
--- src/pages/Detail/styles.js
+++ src/pages/Detail/styles.js
@@ -25,4 +25,50 @@ justify-content: center;
 align-items: center;
 `;
 
-export const Banner = styled.Image``;
+export const Banner = styled.Image`
+width: 100%;
+height: 350px;
+border-bottom-left-radius: 70px;
+border-bottom-right-radius: 70px;
+`; 
+
+export const ButtonLink = styled.TouchableOpacity`
+background-color: #e72f49;
+width: 63px;
+height: 63px;
+border-radius: 35px;
+position: absolute;
+top: 300px;
+right: 15px;
+justify-content: center;
+align-items: center;
+z-index: 99;
+`;
+
+export const Title = styled.Text`
+color: #fff;
+font-size: 22px;
+font-weight: bold;
+padding: 8px 14px;
+margin-top: 8px;
+`;
+
+export const ContentArea = styled.View`
+flex-direction: row;
+align-items: center;
+padding: 0 14px;
+justify-content:space-between;
+`;
+
+export const Rate = styled.Text`
+font-size: 18px;
+font-weight: bold;
+color: #fff;
+`;
+
+export const ListGenres = styled.FlatList`
+padding-left: 14px;
+margin: 8px 0;
+max-height: 35px;
+min-height: 35px;   
+`;
